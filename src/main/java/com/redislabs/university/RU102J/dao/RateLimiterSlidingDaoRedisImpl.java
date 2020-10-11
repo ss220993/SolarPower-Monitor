0c70b8dc04243bf99a6d302a04b2b4f8614656fb
Subhashree Parthasarathy <subha220993@gmail.com>
2020-10-11 17:05:16 +0530

    Implement sliding window rate limiter

--- src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
@@ -1,6 +1,13 @@
 package com.redislabs.university.RU102J.dao;
 
+import java.time.ZonedDateTime;
+
+import com.redislabs.university.RU102J.core.KeyHelper;
+
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
 
 public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
 
@@ -19,6 +26,21 @@ public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
     @Override
     public void hit(String name) throws RateLimitExceededException {
         // START CHALLENGE #7
+        try(Jedis jedis = jedisPool.getResource()){
+            String key = KeyHelper.getKey("limiter:" + windowSizeMS + ":" + name + ":" + maxHits);
+            long now = ZonedDateTime.now().toInstant().toEpochMilli();
+
+            Transaction trans = jedis.multi();
+            String keyValue = now + "-" + Math.random();
+            trans.zadd(key, now, keyValue);
+            trans.zremrangeByScore(key, 0, now - windowSizeMS);
+            Response<Long> hits = trans.zcard(key);
+            trans.exec();
+
+            if (hits.get() > maxHits) {
+                throw new RateLimitExceededException();
+            }
+        }
         // END CHALLENGE #7
     }
 }
--- src/test/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImplTest.java
+++ src/test/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImplTest.java
@@ -47,7 +47,6 @@ public class RateLimiterSlidingDaoRedisImplTest {
         keyManager.deleteKeys(jedis);
     }
 
-    @Ignore
     @Test
     public void hit() {
         int exceptionCount = 0;
@@ -64,7 +63,6 @@ public class RateLimiterSlidingDaoRedisImplTest {
         assertThat(exceptionCount, is(0));
     }
 
-    @Ignore
     @Test
     public void hitOutsideLimit() {
         int exceptionCount = 0;
@@ -80,8 +78,7 @@ public class RateLimiterSlidingDaoRedisImplTest {
 
         assertThat(exceptionCount, is(2));
     }
-
-    @Ignore
+    
     @Test
     public void hitOutsideWindow() throws InterruptedException {
         int exceptionCount = 0;
